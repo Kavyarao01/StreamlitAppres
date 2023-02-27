74502a82e09ffe033a233ee04d1c5cf321c46499
Kavyarao01 <110031619+Kavyarao01@users.noreply.github.com>
2023-02-27 14:35:51 +0530

    Add files via upload

--- pages/My Location.py
+++ pages/My Location.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pydeck as pdk
 import pandas as pd
+from matplotlib import image
+import os
+import pandas as pd
+import plotly.express as px
 
 
 st.header("View My Location")
-# read in data
-df = pd.read_csv(r'C:/Users/kavya/OneDrive/Desktop/Innomatics internship/assgnapp/resources/data/sample.csv', sep=',')
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+DATA_PATH = os.path.join(dir_of_interest, "data", "sample.csv")
+
+
+df = pd.read_csv(DATA_PATH)
 
 layer = pdk.Layer(
         "ScatterplotLayer",
