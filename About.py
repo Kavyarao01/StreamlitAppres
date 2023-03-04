7babaf080658e1a6c561e1b470bb1211b632741a
Kavyarao01 <110031619+Kavyarao01@users.noreply.github.com>
2023-03-04 19:55:12 +0530

    Add files via upload

--- About.py
+++ About.py
@@ -2,11 +2,11 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: Black;'>Hello Everyone! </h1>", unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: left; color: Red;'>Kavya Rao </h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: left; color: black;'>Data Science Enthusiast </h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: left; color: black;'>Data Science Intern </h2>", unsafe_allow_html=True)
 
 
 
-st.markdown("I am Kavya . Data science enthusiast Looking for New Challenges/Opportunities in the field of Data  Science and an aspiring Data scientist who enjoys connecting the dots: be it ideas from different disciplines, people from different teams, or applications from different industries.I have strong technical skills and an academic background in engineering, statistics, and machine learning.")
+st.markdown("I am Kavya . Data science Intern Looking for New Challenges/Opportunities in the field of Data  Science and an aspiring Data scientist who enjoys connecting the dots: be it ideas from different disciplines, people from different teams, or applications from different industries.I have strong technical skills and an academic background in engineering, statistics, and machine learning.")
 
 
 
